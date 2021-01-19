8611a42b8ed704f6aa811dff8743b750dcdde716
Rocco Orefice <rocco.orefice92@gmail.com>
2021-01-19 15:02:14 +0100

    HEADER: navbar

---
+++ assets/js/main.js
@@ -0,0 +1,14 @@
+// istanza Vue
+let app = new Vue({
+    el: "#app",
+    data: {
+        logoNavbar: "./assets/img/avada-movers-logo.png",
+        menuNavbar: [
+            "Home",
+            "Rates",
+            "Testimonials",
+            "FAQ",
+            "Contact"
+        ],
+    }
+});
